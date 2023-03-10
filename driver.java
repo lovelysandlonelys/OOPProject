b4c8f45e9df178664084ca4c57d4537b9f4d5cfd
jennifervasquez134 <56005070+jennifervasquez134@users.noreply.github.com>
2023-03-10 00:12:09 -0600

    driver.java changes

--- driver.java
+++ driver.java
@@ -1,31 +1,32 @@
-package myjavapackage;
+package myFirstPackage;
 
 import java.util.Scanner;
 
 public class driver {
 
 	public static void main(String[] args) {
-		
+
 		//Testing Month class
-		Month m1=new Month();
-		System.out.println("Number of days: "+m1.getnumOfDays());
-		System.out.println("Weeks: "+m1.getWeeks());
-		System.out.println("Free time available: "+m1.getFreeTime());
-		m1.displayWeek();
-		m1.displayMonth();
+		MonthClass m1=new MonthClass();
+		System.out.println("Number of days: "+m1.getNumberOfDaysInMonth(2,2002));
+		System.out.println("Hello! Today is: "+m1.displayMonthName(2));
+		m1.setMonth();
+		System.out.println("Hello! Today is: "+m1.displayMonthName(m1.getMonth()));
+		
+		//System.out.println("Free time available: "+m1.getFreeTime());
 		
 		//Testing Day class
-		Day d1=new Day();
-		System.out.println("Free time available: "+d1.getFreeTime());
-		d1.displayDay();
-		d1.addEvent();
-		
-		
+		//Day d1=new Day();
+		//System.out.println("Free time available: "+d1.getFreeTime());
+		//d1.displayDay();
+		//d1.addEvent();
+
+
 		//Testing Week class
-		Week w1=new Week();
-		System.out.println("Free time available: "+w1.getFreeTime());
-		
-		
+		//Week w1=new Week();
+		//System.out.println("Free time available: "+w1.getFreeTime());
+
+
 		//Testing Event class and making sure the function work correctly
 		Event e1=new Event();
 		Event e2=new Event();
@@ -34,38 +35,38 @@ public class driver {
 		e1.setDescription("Event actually starts at 6:30");
 		e1.setLocation("TBA");
 		e1.displayEvent();
-		
-		
+
+
 		//Testing FreeTime class
 		FreeTime object1=new FreeTime();
 		FreeTime object2=new FreeTime();
-		System.out.println("Free time available "+object1.getTimeFree());
-		object1.displayTimeFree();
-		
-		object2.minusTimeFree(15);
-		object2.displayTimeFree();
-		
-		
+		//System.out.println("Free time available "+object1.getTimeFree());
+		//object1.displayTimeFree();
+
+		//object2.minusTimeFree(15);
+		//object2.displayTimeFree();
+
+
 		//Testing ToDoList
-		ToDoList a1=new ToDoList();
-		a1.addUnfinishedTask();
-		a1.deleteTask();
-		
+		//ToDoList a1=new ToDoList();
+		//a1.addUnfinishedTask();
+		//a1.deleteTask();
+
 		//Testing UnfinishedToDo class
-		UnfinishedToDo obj1=new UnfinishedToDo();
-		obj1.changeTask("Do Laundry");
-		obj1.deleteTask();
-		
+		//UnfinishedToDo obj1=new UnfinishedToDo();
+		//obj1.changeTask("Do Laundry");
+		//obj1.deleteTask();
+
 		//Testing FinishedToDo Class
-		FinishedToDo d1=new UnfinishedToDo();
-		d1.deleteTask();
-		
-	
+		//FinishedToDo d1=new UnfinishedToDo();
+		//d1.deleteTask();
+
+
 		//Testing View Class
-		View v1=new View();
-		v1.displayMonthly();
-		v1.displayWeekly();
-		v1.displayDaily();
+		//View v1=new View();
+		//v1.displayMonthly();
+		//v1.displayWeekly();
+		//v1.displayDaily();
 	}
 
 }
