4ecbdf34bc43a3c12e593c4a9eacc993621598d3
RankedGabe <93612070+RankedGabe@users.noreply.github.com>
2023-03-10 09:54:37 -0600

    Update Week.java
    Added comments.

--- Week.java
+++ Week.java
@@ -1,51 +1,77 @@
 package maintaincontrolpackage;
 
-// Currently, this class works assuming the calculations for the weekly free time/time used will happen elsewhere like in Main.
+// I see this class being utilized each time the user would like to add an event. Once they wish to add an event, we can 
+// ask the user for the private attributes
 
-// CANNOT CALCULATE THE WEEKLY EVENT FREE TIME BECAUSE I CANNOT ACCESS THE Days arrays Events array (The classes don't inherit one another
-// SO I cannot use the dot operator.
-
-// Solutions:
-// 1. Inheritance
-// 2. In day class, have a function that sends the events array per day to the week class for the freeTime to be added together
-// 3. Entirely remove the Week class and calculate the weekly information via the amount of days in the Day class (easier access to
-// member data
-
-public class Week {
-
-// Class attributes:
-	private String name;
-	private FreeTime weeklyFreeTime;
+// How the following will interact with the GUI is currently unknown. We may have to add statements like "The event name has been updated." 
+// into the actual functions themselves below.
+public class Event {
 	
-// Class methods:
+// Class attributes
+	private String name; // Name of event
+	private float time; // Hours allocated for task
+	private String description; // Description of event
+	private String location; // Location of event
+	
+// Class methods
 	
 	// Constructor
-	public Week(Day days[]),
-	{
-		name = "Week 1";
-		
-		float total = 0.0f;
-		for (int i = 0; i < days.length; i++)
-		{
-			total = days.
-		}
+	public Event() {
+		name = "N/A";
+		time = 0.0f;
+		description = "N/A";
+		location = "N/A";
 	}
+
+// Name attribute setters/getters
 	
-	public String getName()
-	{
+	public String getName() {
 		return name;
 	}
 	
-	public FreeTime getWeeklyFreeTime()
-	{
-		return weeklyFreeTime;
+	public void setName(String name) {
+		this.name = name;
 	}
 	
-	public void displayWeek() 
-	{
-		// Output the name of the week
-		System.out.println(name);
-		System.out.println("This weeks time free: " + freeTime.getTimeFree());
-		System.out.println("This weeks time used: " + freeTime.getTimeUsed());
+// Time attribute setters/getters
+	
+	public float getTime() {
+		return time;
+	}
+	
+	public void setTime(float time) {
+		this.time = time;
+	}
+	
+// Description attribute setter/getters
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	// When inputting the description, make sure the input can account for spaces and not just singular words (syntax note for later)
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+// Location attribute setter/getters
+	
+    // When inputting the location, make sure the input can account for spaces and not just singular words (syntax note for later)
+	public String getLocation() {
+		return location;
+	}
+	
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+
+// Displays event information:
+	// May have to turn into returnable string
+	public void displayEvent() {
+		System.out.println("Event: " + name);
+		System.out.println("Time of event: " + time);
+		System.out.println("Event description: " + description);
+		System.out.println("Event location: " + location);
 	}
 }
